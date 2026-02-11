626abeb69adda3eada9dfd4825f5871789ba2eee
Tutul <tutulkabir@gmail.com>
2026-02-11 13:38:53 +0530

    async handler, response handler, globalErrorhandler middleware, notFound route added

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,19 +1,20 @@
 import type { NextFunction, Request, Response } from "express";
 import { UserService } from "./user.service";
 import { StatusCodes } from "http-status-codes";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const user = await UserService.createUser(req.body);
-    res.status(StatusCodes.CREATED).json({
+    sendResponse(res, {
+      success: true,
+      statusCode: StatusCodes.CREATED,
       message: "User created successfully",
-      user,
+      data: user,
     });
-  } catch (error: any) {
-    console.log(error);
-    next(error);
-  }
-};
+  },
+);
 
 export const UserController = {
   createUser,
